0d4879a884f38e0eaa3e206a731e157d9b7aade2
Kaio <kaiohtf38@gmail.com>
2024-10-28 23:00:37 -0300

    update

---
+++ nutriFAGOC/src/main/kotlin/br/com/nutriFAGOC/NutriFagocApplication.kt
@@ -0,0 +1,11 @@
+package br.com.nutriFAGOC
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class NutriFagocApplication
+
+fun main(args: Array<String>) {
+	runApplication<NutriFagocApplication>(*args)
+}
---
+++ nutriFAGOC/src/test/kotlin/br/com/nutriFAGOC/NutriFagocApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.com.nutriFAGOC
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class NutriFagocApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
